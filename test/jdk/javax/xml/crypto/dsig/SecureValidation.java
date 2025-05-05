27b7744d84d807c1620ac9eb11f95676cac6e6f4
JinhangZhang <Jinhang.Zhang@ibm.com>
2025-05-05 16:02:59 -0400

    Tolerate RSA-PSS in OpenJCEPlus signature

--- test/jdk/javax/xml/crypto/dsig/SecureValidation.java
+++ test/jdk/javax/xml/crypto/dsig/SecureValidation.java
@@ -31,6 +31,13 @@
  *          java.base/sun.security.x509
  * @run main/othervm SecureValidation
  */
+
+/*
+ * ===========================================================================
+ * (c) Copyright IBM Corp. 2025, 2025 All Rights Reserved
+ * ===========================================================================
+ */
+
 import jdk.test.lib.Asserts;
 import jdk.test.lib.security.XMLUtils;
 import jdk.test.lib.Utils;
@@ -66,8 +73,21 @@ public class SecureValidation {
         g.generate(2048);
         X509Certificate cert = g.getSelfCertificate(new X500Name("CN=Me"), 100);
         PrivateKey privateKey = g.getPrivateKey();
-        PSSParameterSpec pspec = new PSSParameterSpec("SHA-384", "MGF1",
+        try {
+            PSSParameterSpec pspec = new PSSParameterSpec("SHA-384", "MGF1",
                 MGF1ParameterSpec.SHA512, 48, TRAILER_FIELD_BC);
+        } catch (javax.xml.crypto.dsig.XMLSignatureException xmlse) {
+            Throwable cause = xmlse.getCause();
+            if (cause instanceof java.security.InvalidAlgorithmParameterException) {
+                if (Signature.getInstance("RSA-PSS").getProvider().getName().equals("OpenJCEPlus")
+                && cause.getMessage().equals("The message digest within the PSSParameterSpec does not match the MGF message digest.")
+                ) {
+                    System.out.println("Expected error message is caught for OpenJCEPlus provider.");
+                    return;
+                }
+            }
+            throw xmlse;
+        }
 
         // Sign with PSS with SHA-384 and SHA-512
         Document signed = XMLUtils.signer(privateKey, cert)
