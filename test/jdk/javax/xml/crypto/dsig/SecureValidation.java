baa39255044f3ceeacd2e025fc7b15b377059005
JinhangZhang <Jinhang.Zhang@ibm.com>
2025-05-06 00:51:34 -0400

    update

--- test/jdk/javax/xml/crypto/dsig/SecureValidation.java
+++ test/jdk/javax/xml/crypto/dsig/SecureValidation.java
@@ -76,28 +76,26 @@ public class SecureValidation {
         PrivateKey privateKey = g.getPrivateKey();
         PSSParameterSpec pspec = new PSSParameterSpec("SHA-384", "MGF1",
                 MGF1ParameterSpec.SHA512, 48, TRAILER_FIELD_BC);
-        // PSSParameterSpec pspec;
-        // try {
-        //     pspec = new PSSParameterSpec("SHA-384", "MGF1",
-        //         MGF1ParameterSpec.SHA512, 48, TRAILER_FIELD_BC);
-        // } catch (javax.xml.crypto.dsig.XMLSignatureException xmlse) {
-        //     Throwable cause = xmlse.getCause();
-        //     if (cause instanceof java.security.InvalidAlgorithmParameterException) {
-        //         if (Signature.getInstance("RSA-PSS").getProvider().getName().equals("OpenJCEPlus")
-        //         && cause.getMessage().equals("The message digest within the PSSParameterSpec does not match the MGF message digest.")
-        //         ) {
-        //             System.out.println("Expected error message is caught for OpenJCEPlus provider.");
-        //             return;
-        //         }
-        //     }
-        //     throw xmlse;
-        // }
 
         // Sign with PSS with SHA-384 and SHA-512
-        Document signed = XMLUtils.signer(privateKey, cert)
+        Document sign = XMLUtils.signer(privateKey, cert)
                 .dm(DigestMethod.SHA384)
-                .sm(SignatureMethod.RSA_PSS, new RSAPSSParameterSpec(pspec))
-                .sign(doc);
+                .sm(SignatureMethod.RSA_PSS, new RSAPSSParameterSpec(pspec));
+		Document signed;
+        try {
+            signed = sign.sign(doc);
+        } catch (javax.xml.crypto.dsig.XMLSignatureException xmlse) {
+            Throwable cause = xmlse.getCause();
+            if (cause instanceof java.security.InvalidAlgorithmParameterException) {
+                if (Signature.getInstance("RSA-PSS").getProvider().getName().equals("OpenJCEPlus")
+                && cause.getMessage().equals("The message digest within the PSSParameterSpec does not match the MGF message digest.")
+                ) {
+                    System.out.println("Expected error message is caught for OpenJCEPlus provider.");
+                    return;
+                }
+            }
+            throw xmlse;
+        }
 
         XPath xp = XPathFactory.newInstance().newXPath();
         xp.setNamespaceContext(new NamespaceContext() {
